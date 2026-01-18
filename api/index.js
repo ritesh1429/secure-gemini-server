8b4084c0af4b108ae6c1c244894847d89b35ad53
Ritesh Kumar <ritesh813222@gmail.com>
2026-01-18 18:51:47 +0530

    Switch to gemini-1.5-flash

--- api/index.js
+++ api/index.js
@@ -17,8 +17,8 @@ export default async function handler(req, res) {
     const genAI = new GoogleGenerativeAI(apiKey);
     
     // 👇👇 YAHAN CHANGE KIYA HAI 👇👇
-    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-
+    //const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const systemInstruction = `
     Generate a project file structure based on the prompt.
     Return ONLY a JSON array: [{"filename": "...", "code": "..."}].
